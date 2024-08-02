4e530eba9d41f763acad76599f8b873ff1757747
Gonzalo Hamet Loza Quispe <79913828+YoxD1234@users.noreply.github.com>
2024-08-02 10:55:56 -0500

    Delete proyecto1 directory

--- proyecto1/js/script.js
+++
@@ -1,9 +0,0 @@
-const card = document.querySelector('.card');
-const body = document.querySelector('body');
-
-card.addEventListener("mouseenter",(e)=>{
-    body.style.backdropFilter='blur(0px)';
-})
-card.addEventListener("mouseleave", (e)=>{
-    body.style.backdropFilter='blur(6px)';
-})
